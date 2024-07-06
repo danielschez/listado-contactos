c9aa70e76827739028708c9acac5072b63dd92b7
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-07-06 16:10:25 -0600

    Se agregan las url y detalles de la vista de contasctos

--- catalogo_contacto/urls.py
+++ catalogo_contacto/urls.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
+
 from django.urls import path
+from django.urls import include
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 from . import views
 
+from contacts.views import ContactListView
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', ContactListView.as_view(), name='index'),
     path('usuarios/login', views.login_view, name='login'),
     path('usuarios/logout', views.logout_view, name='logout'),
     path('usuarios/registro', views.register, name='register'),
     path('admin/', admin.site.urls),
+    path('contactos/', include('contacts.urls'))
 ]
 
 if settings.DEBUG:
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,26 @@
+from typing import Any
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Contacto
+
+class ContactListView(ListView):
+    template_name = 'index.html'
+    queryset = Contacto.objects.all().order_by('id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Listados de contactos'
+        context['contactos'] = context['contacto_list']
+        return context
+    
+class ContactDetailView(DetailView): #id ->pk
+    model = Contacto
+    template_name = 'contacts/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print (context)
+        return context
