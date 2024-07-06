77ad340701f8d1d91ffe6698fae74f929f1ef936
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-07-05 23:29:55 -0600

    Creación de formulario de registro de usuarios y validaciones

--- catalogo_contacto/views.py
+++ catalogo_contacto/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
+
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate 
+
+from django.contrib.auth.models import User
+
+from .forms import RegisterForm
 
 def index(request):
     return render(request, 'index.html', {
         'title': 'Sign In' #context
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect('index')
+        else:
+            messages.error(request, 'Usuario o Contraseña incorrecto')
+
+    return render(request, 'users/login.html', {
+        'title': 'Login' #context
+    })
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesión cerrada exitosamente')
+    return redirect('login')
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+       
+        user = form.save()
+        if user:
+            login(request, user)
+            messages.success(request, 'Usuario creado exitosamente')
+            return redirect('index')
+
+    return render(request, 'users/register.html', {
+        'form': form
     })
