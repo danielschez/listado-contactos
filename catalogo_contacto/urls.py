a76ff12b38e512db91b9cce4d5dbe45d9bda41ff
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-07-06 14:44:58 -0600

    Configuración de imagenes para mostrar

--- catalogo_contacto/urls.py
+++ catalogo_contacto/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from . import views
 
 urlpatterns = [
@@ -10,3 +13,6 @@ urlpatterns = [
     path('usuarios/registro', views.register, name='register'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
