3cda83f561f690bb501d4fc9c3f8c0c32cc11763
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-07-07 21:58:58 -0600

    Reapply "Buscardor de vista de registros"
    This reverts commit de64926acb608cf079058c8c5772a3bd25f754a6.

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'contacts'
 urlpatterns = [
+    path('search', views.ContactSearchListView.as_view(), name='search'),
     path('<pk>', views.ContactDetailView.as_view(), name='contact'), #id -> llave primaria
 ]
