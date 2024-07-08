8947d8c7c818a7534c1b5e8d7bb0c0b0160a20fd
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-07-07 18:44:56 -0600

    Modificación de datos a mostrar en el administrador

--- contacts/admin.py
+++ contacts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Contacto
 
-admin.site.register(Contacto)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellidoPaterno', 'apellidoMaterno', 'telefono', 'email')
+    search_fields = ('nombre', 'apellidoPaterno', 'apellidoMaterno', 'telefono', 'email')
+
+admin.site.register(Contacto, ContactAdmin)
